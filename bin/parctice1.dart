79cc8bfeb7a1e3bc8ebb63a98540e76e073fd5b7
Rimon <saifullahmahmud.hstu@gmail.com>
2022-02-12 22:19:27 +0600

    first add

---
+++ bin/parctice1.dart
@@ -0,0 +1,22 @@
+void main(List<String> arguments) {
+  int value1 = 200;
+  int value2 =100;
+
+  int sum = value1+value2;
+
+  print(sum);
+
+  int sub = value1-value2;
+
+  print(sub);
+
+  int mul = value1*value2;
+
+  print(mul);
+
+  double div = (value1/value2);
+
+  print('$value1 / $value2 = $div');
+
+
+}
